92c4a302b3209c92f98d475e91b5e549a729702a
HenryMM <henfismat@gmail.com>
2022-12-20 10:15:55 -0500

    Creación de los test para carga inicial de marcas.

---
+++ creditoauto.Test/Infraestructura/Services/MarcaInfraestructuraTest.cs
@@ -0,0 +1,88 @@
+﻿using creditoauto.Common.ClassMaps;
+using creditoauto.Domain.Interfaces;
+using creditoauto.Entity.Models;
+using creditoauto.Infraestructure.Services;
+using Microsoft.Extensions.Configuration;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace creditoauto.Test.Infraestructura.Services
+{
+    public class MarcaInfraestructuraTest
+    {
+        [Test]
+        public async Task CargaInicialAsync_MarcasRepetidas_RemoverElementosDuplicados()
+        {
+            #region Arrange
+            string ubicacionArchivo = "C:\\Users\\PC\\UbicacionDefault";
+            int expectedCount = 2;
+            var _marcaRepository = new Mock<IRepository<Marca>>();
+            var _fileHelper = new Mock<IFileHelper<Marca>>();
+            var _config = new Mock<IConfiguration>();
+
+            List<Marca> marcasFake = new List<Marca>
+            {
+                new Marca
+                {
+                    Nombre = "Marca1"
+                },
+                new Marca
+                {
+                    Nombre = "Marca2"
+                },
+                new Marca {
+                    Nombre = "Marca2"
+                }
+            };
+
+            _config.Setup(p => p.GetSection(It.IsAny<string>()).Value).Returns(ubicacionArchivo);
+            _fileHelper.Setup(f => f.LeerArchivoCSV<MarcaMap>(It.IsAny<string>())).Returns(marcasFake);
+
+
+            var target = new MarcaInfraestructura(_marcaRepository.Object,
+               _config.Object, _fileHelper.Object);
+            #endregion
+
+            #region Act
+            var clientesResult = await target.CargaInicialAsync();
+            #endregion
+
+            #region Assert
+            Assert.That(expectedCount, Is.EqualTo(clientesResult.Data.Count));
+            #endregion
+        }
+
+        [Test]
+        public async Task CargaInicialAsync_CantidadMarcasEsCero_ReturnListaVacia()
+        {
+            #region Arrange
+            string ubicacionArchivo = "C:\\Users\\PC\\UbicacionDefault";
+            int expectedCount = 0;
+            var _marcaRepository = new Mock<IRepository<Marca>>();
+            var _fileHelper = new Mock<IFileHelper<Marca>>();
+            var _config = new Mock<IConfiguration>();
+
+            List<Marca> marcasFake = new List<Marca>{};
+
+            _config.Setup(p => p.GetSection(It.IsAny<string>()).Value).Returns(ubicacionArchivo);
+            _fileHelper.Setup(f => f.LeerArchivoCSV<MarcaMap>(It.IsAny<string>())).Returns(marcasFake);
+
+
+            var target = new MarcaInfraestructura(_marcaRepository.Object,
+               _config.Object, _fileHelper.Object);
+            #endregion
+
+            #region Act
+            var clientesResult = await target.CargaInicialAsync();
+            #endregion
+
+            #region Assert
+            Assert.That(expectedCount, Is.EqualTo(clientesResult.Data.Count));
+            #endregion
+        }
+    }
+}
--- creditoauto.Test/Infraestructura/Services/SolicitudCreditoTest.cs
+++ creditoauto.Test/Infraestructura/Services/SolicitudCreditoTest.cs
@@ -55,54 +55,5 @@ namespace creditoauto.Test.Infraestructura.Services
             Assert.That(actualResult.Mensaje, Is.EqualTo(excpectedMessage));
             #endregion
         }
-
-        //[Test]
-        //public async Task CrearSolicitudCredito_VehiculoTieneSolicitud_MensajeError()
-        //{
-        //    #region Arrange
-        //    string excpectedMessage = "El vehiculo tiene una solicitud registrada. No puede ser vendido.";
-        //    int clienteId = 1;
-        //    int vehiculoId = 1;
-        //    var _solicitudCreditoRepository = new Mock<IRepository<SolicitudCredito>>();
-        //    var _ejecutivoRepository = new Mock<IRepository<Ejecutivo>>();
-        //    var _clientePatioRepository = new Mock<IRepository<ClientePatio>>();
-        //    ISolicitudCreditoInfraestructura _target = new SolicitudCreditoInfraestructura(_solicitudCreditoRepository.Object,
-        //        _ejecutivoRepository.Object, _clientePatioRepository.Object);
-
-
-        //    IQueryable<SolicitudCredito> solicitudCreditosFake = new List<SolicitudCredito>
-        //    {
-        //        new SolicitudCredito
-        //        {
-        //             ClienteId = clienteId,
-        //            Cuotas = 2,
-        //            EjecutivoId = 2,
-        //            Entrada = 3,
-        //            Estado = "REGISTRADO"
-        //        }
-        //    }.AsQueryable();
-
-        //    _solicitudCreditoRepository.Setup(
-        //        m => m.SearchByAsync(x => x.VehiculoId == vehiculoId && x.Estado == "REGISTRADO")).ReturnsAsync(solicitudCreditosFake);
-
-        //    SolicitudCredito solicitudCredito = new SolicitudCredito
-        //    {
-        //        ClienteId = clienteId,
-        //        Cuotas = 2,
-        //        EjecutivoId = 2,
-        //        Entrada = 3,
-        //        Estado = "REGISTRADO",
-        //        VehiculoId = vehiculoId
-        //    };
-        //    #endregion
-
-        //    #region Act
-        //    var actualResult = await _target.CrearSolicitudCredito(solicitudCredito);
-        //    #endregion
-
-        //    #region Assert
-        //    Assert.That(actualResult.Mensaje, Is.EqualTo(excpectedMessage));
-        //    #endregion
-        //}
     }
 }
